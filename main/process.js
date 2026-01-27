77ff047105192c65996eedd68f095c87c7662f2f
Yuzu-Lemon0822 <yuzulemon140416@gmail.com>
2026-01-27 12:58:50 +0900

    ud

--- main/process.js
+++ main/process.js
@@ -19,54 +19,52 @@ export function main() {
     }
     basisData.canvasX = basisData.drag_basisX + (pointer.pinch.centerX - pointer.pinch.startCenterX);
     basisData.canvasY = basisData.drag_basisY + (pointer.pinch.centerY - pointer.pinch.startCenterY);
-    basisData.scale = basisData.drag_basisScale + (pointer.pinch.dist - pointer.pinch.startDist) / 30
+    basisData.scale = Math.max(0, basisData.drag_basisScale + (pointer.pinch.dist - pointer.pinch.startDist) / 30)
     pinchTimer++
     return; //早期処理中断
   } else {
     pinchTimer = 0;
-  }
-
-  if (pointer.down) {
-    pointerTimer++
-  } else {
-    pointerTimer = 0;
-  }
-
-  if (basisData.mode === "move") {
     if (pointer.down) {
-      if (pointerTimer === 1) {
-        basisData.drag_basisX = basisData.canvasX;
-        basisData.drag_basisY = basisData.canvasY;
-        basisData.virtualOriginX = pointer.x;
-        basisData.virtualOriginY = pointer.y;
-      }
-      basisData.canvasX = basisData.drag_basisX + (pointer.x - basisData.virtualOriginX);
-      basisData.canvasY = basisData.drag_basisY + (pointer.y - basisData.virtualOriginY);
-    }
-  } else if (basisData.mode === "draw") {
-    if (pointer.down) {
-      const x = Math.floor((pointer.x - basisData.canvasX) / basisData.scale);
-      const y = Math.floor((pointer.y - basisData.canvasY) / basisData.scale);
-
-      const key = `${x},${y}`;
-
-      if (
-        0 <= x && x < basisData.canvasWidth &&
-        0 <= y && y < basisData.canvasHeight &&
-        !drawTemp.has(key)
-      ) {
-        drawTemp.set(key, { x, y });
-      }
+      pointerTimer++
+    } else {
+      pointerTimer = 0;
     }
 
-    if (!pointer.down && drawTemp.size > 0) { 
-      for (const { x, y } of drawTemp.values()) {
-        canvasData.canvas1.layer1[y][x] = "#000000";
+    if (basisData.mode === "move") {
+       if (pointer.down) {
+        if (pointerTimer === 1) {
+          basisData.drag_basisX = basisData.canvasX;
+          basisData.drag_basisY = basisData.canvasY;
+          basisData.virtualOriginX = pointer.x;
+          basisData.virtualOriginY = pointer.y;
+        }
+        basisData.canvasX = basisData.drag_basisX + (pointer.x - basisData.virtualOriginX);
+        basisData.canvasY = basisData.drag_basisY + (pointer.y - basisData.virtualOriginY);
       }
-      drawTemp.clear();
+    } else if (basisData.mode === "draw") {
+      if (pointer.down) {
+        const x = Math.floor((pointer.x - basisData.canvasX) / basisData.scale);
+        const y = Math.floor((pointer.y - basisData.canvasY) / basisData.scale);
+
+        const key = `${x},${y}`;
+
+        if (
+          0 <= x && x < basisData.canvasWidth &&
+          0 <= y && y < basisData.canvasHeight &&
+          !drawTemp.has(key)
+        ) {
+          drawTemp.set(key, { x, y });
+        }
+      }
+
+      if (!pointer.down && drawTemp.size > 0) { 
+        for (const { x, y } of drawTemp.values()) {
+          canvasData.canvas1.layer1[y][x] = "#000000";
+        }
+        drawTemp.clear();
+      }
     }
   }
-
   basisData.display_minX = displayClipping(Math.floor((0 - basisData.canvasX) / basisData.scale), basisData.canvasWidth - 1);
   basisData.display_maxX = displayClipping(Math.ceil((basisData.width - basisData.canvasX) / basisData.scale), basisData.canvasWidth - 1);
   basisData.display_minY = displayClipping(Math.floor((0 - basisData.canvasY) / basisData.scale), basisData.canvasHeight - 1);
