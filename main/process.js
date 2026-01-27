ce36c98f55e50427e3719e8953936ba392fa793b
Yuzu-Lemon0822 <yuzulemon140416@gmail.com>
2026-01-27 20:00:58 +0900

    ud

--- main/process.js
+++ main/process.js
@@ -7,10 +7,12 @@ function displayClipping(x, max) {
 
 let pointerTimer = 0;
 let pinchTimer = 0;
+let previousX = null;
+let previousY = null;
 export const drawTemp = new Map()
 
 function createLine(x1, y1, x2, y2) {
-  const samplingNum = Math.abs(x2 - x1) + Math.abs(y2- y1);
+  const samplingNum = Math.max(1, Math.abs(x2 - x1) + Math.abs(y2- y1));
   const dx = (x2 - x1) / samplingNum;
   const dy = (y2 - y1) / samplingNum;
   const pointList = []
@@ -18,23 +20,31 @@ function createLine(x1, y1, x2, y2) {
   let y = y1
   let lastPoint = []
   for (let i = 0; i <= samplingNum; i++) {
-    if (lastPoint.length === 0 || (lastPoint[0] === Math.floor(x) && lastPoint[1] === Math.floor(y) /*重複回避*/)) {
+    if (lastPoint.length === 0 || lastPoint[0] !== Math.floor(x) || lastPoint[1] !== Math.floor(y) /*重複回避*/) {
       lastPoint = [Math.floor(x), Math.floor(y)]
       pointList.push(lastPoint)
     }
     x += dx
     y += dy
   }
-  return pointList
+  return pointList;
 }
 
 function refineLine(lineData) {
   const pointList = []
   for (const point of lineData) {
-    if (pointList.length < 3) {
-
+    if (pointList.length > 1) {
+      const p0 = point
+      const p2 = pointList[pointList.length - 2]
+      if (Math.abs(p0[0] - p2[0]) + Math.abs(p0[1] - p2[1]) === 2) {
+        if (p0[0] !== p2[0] && p0[1] !== p2[1]) {
+          pointList.pop()
+        }
+      }
     }
+    pointList.push(point)
   }
+  return pointList;
 }
 
 export function main() {
@@ -93,30 +103,37 @@ export function main() {
       }
     } else if (basisData.mode === "draw") {
       if (pointer.down) {
-        const x = Math.floor(
-          (pointer.x - basisData.canvasX) / basisData.scale
-        );
-        const y = Math.floor(
-          (pointer.y - basisData.canvasY) / basisData.scale
-        );
+        const x = (pointer.x - basisData.canvasX) / basisData.scale;
+        const y = (pointer.y - basisData.canvasY) / basisData.scale;
+      
+        if (previousX !== null) {
+          const pointList = refineLine(createLine(previousX, previousY, x, y))
+          for (const point of pointList) {
+            const x = point[0];
+            const y = point[1];
+            const key = `${x},${y}`;
 
-        const key = `${x},${y}`;
+            if ((0 <= x && x < basisData.canvasWidth) && (0 <= y && y < basisData.canvasHeight) && !drawTemp.has(key)) {
+              drawTemp.set(key, { x, y });
+            }
+          }
+        }
 
-        if (
-          0 <= x && x < basisData.canvasWidth &&
-          0 <= y && y < basisData.canvasHeight &&
-          !drawTemp.has(key)
-        ) {
-          drawTemp.set(key, { x, y });
+        previousX = x
+        previousY = y
+      }
+
+      if (!pointer.down) {
+        previousX = null;
+
+        if (drawTemp.size > 0) {
+          for (const { x, y } of drawTemp.values()) {
+            canvasData.canvas1.layer1[y][x] = "#000000";
+          }
+          drawTemp.clear();
         }
       }
 
-      if (!pointer.down && drawTemp.size > 0) {
-        for (const { x, y } of drawTemp.values()) {
-          canvasData.canvas1.layer1[y][x] = "#000000";
-        }
-        drawTemp.clear();
-      }
     }
   }
 
